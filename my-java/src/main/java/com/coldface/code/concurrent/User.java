8029a455a6170afdc199d98d8fb70f6cf4110b6d
关心 <jack.zhao829@gmail.com>
2015-10-31 15:07:23 +0800

    add java code

--- my-java/src/main/java/com/coldface/code/concurrent/User.java
+++ my-java/src/main/java/com/coldface/code/concurrent/User.java
@@ -18,8 +18,9 @@ public class User {
 	}
 	
 	/**
-	 * 业务方法
+	 * 业务方法(同步方法)
 	 * @param 添加x元
+	 * 注意：在使用synchronized关键字的时候，尽可能避免在synchronized方法或者块中使用sleep或者yield方法，因为synchronized程序块占着对象睡觉，严重影响效率。
 	 */
 	public synchronized void oper(int x){
 		try {
@@ -36,6 +37,20 @@ public class User {
 	@Override
 	public String toString(){
 		return "User{code="+code+",cash="+cash+"}";
+	}
+	
+	public void opern(int x){
+		try {
+			Thread.sleep(10L);
+			synchronized(this){
+				this.cash +=x;
+				System.out.println(Thread.currentThread().getName()+"运行结束，增加"+x+"元，当前账号余额为："+cash);
+			}
+	        Thread.sleep(10L);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
