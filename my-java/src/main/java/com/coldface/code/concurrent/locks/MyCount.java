9de5a937e962fa8c596840f2317cded029660cc4
关心 <jack.zhao829@gmail.com>
2015-11-03 21:02:11 +0800

    add java code

---
+++ my-java/src/main/java/com/coldface/code/concurrent/locks/MyCount.java
@@ -0,0 +1,51 @@
+package com.coldface.code.concurrent.locks;
+
+/**
+ * 信用卡账户，可随意透支
+ * @author coldface
+ *
+ */
+public class MyCount {
+
+	private String oid; //账号
+	private int cash; //账号余额
+	
+	MyCount(String oid,int cash){
+		this.oid=oid;
+		this.cash=cash;
+	}
+	
+	
+	
+	public String getOid() {
+		return oid;
+	}
+
+
+
+	public void setOid(String oid) {
+		this.oid = oid;
+	}
+
+
+
+	public int getCash() {
+		return cash;
+	}
+
+
+
+	public void setCash(int cash) {
+		this.cash = cash;
+	}
+
+
+
+	@Override
+	public String toString(){
+		return "MyCount{"+
+				"oid="+oid+
+				",cash="+cash+
+				"}";
+	}
+}
