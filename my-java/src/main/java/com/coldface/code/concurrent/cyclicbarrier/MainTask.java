4a1b19b96af9f6f3847e9bdb1a196e3eaa0949c9
关心 <jack.zhao829@gmail.com>
2015-11-14 20:35:01 +0800

    add java code

---
+++ my-java/src/main/java/com/coldface/code/concurrent/cyclicbarrier/MainTask.java
@@ -0,0 +1,11 @@
+package com.coldface.code.concurrent.cyclicbarrier;
+
+public class MainTask implements Runnable {
+
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println(">>>>>>>>执行主任务");
+
+	}
+
+}
