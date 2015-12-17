8e86d9f6a06a0b3010633772a73ce472116bfcd2
关心 <jack.zhao829@gmail.com>
2015-12-17 21:45:49 +0800

    add java code

---
+++ my-java/src/main/java/com/coldface/code/flow/context/ContextKeyEnum.java
@@ -0,0 +1,13 @@
+package com.coldface.code.flow.context; 
+/** 
+ * 类ContextKeyEnum.java的实现描述：TODO 类实现描述 
+ * @author coldface 2015年12月11日 上午10:24:30 
+ */
+public enum ContextKeyEnum {
+    /**写入信息*/
+    WRITE_STUENTITY_INFO,
+    /**返回处理结果*/
+    STUENTITY_INFO_RESULT;
+
+}
+
