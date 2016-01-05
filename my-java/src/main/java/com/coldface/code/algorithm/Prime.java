b9d54168084d7118aa52ae7fd30824b780e33128
关心 <jack.zhao829@gmail.com>
2016-01-05 21:09:36 +0800

    add java code

--- my-java/src/main/java/com/coldface/code/algorithm/Prime.java
+++ my-java/src/main/java/com/coldface/code/algorithm/Prime.java
@@ -1,33 +1,32 @@
-package com.coldface.code.algorithm;
-
-/**
- * 类Prime.java的实现描述：TODO 判断一个数是否为素数
- * 
- * @author coldface 2015年12月23日 上午11:16:32
+package com.coldface.code.algorithm; 
+/** 
+ * 类Prime.java的实现描述：TODO 判断一个数是否为素数 
+ * @author coldface 2015年12月23日 上午11:16:32 
  */
 public class Prime {
-
-    public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
-            if (isPrime(i)) {
+    
+    public static void main(String[] args){
+        for(int i=1;i<=100;i++){
+            if(isPrime(i)){
                 System.out.println(i);
             }
         }
     }
-
+    
     /**
-     * 用于判断一个数是否为素数，若为素数，返回true，否则返回false * @return
+     * 用于判断一个数是否为素数，若为素数，返回true，否则返回false
+     * * @return
      */
-    public static boolean isPrime(int a) {
-        boolean flag = true;
+    public static boolean isPrime(int a){
+        boolean flag=true;
         //素数不能小于2
-        if (a < 2) {
-            return false;
-        } else {
-            for (int i = 2; i <= Math.sqrt(a); i++) {
+        if(a<2){
+            return false;  
+        }else{
+            for(int i=2;i<=Math.sqrt(a);i++){
                 //若能被整除，说明不是素数，否则是素数
-                if (a % i == 0) {
-                    flag = false;
+                if(a % i ==0){
+                    flag=false;
                     break;
                 }
             }
@@ -36,3 +35,4 @@ public class Prime {
     }
 
 }
+
