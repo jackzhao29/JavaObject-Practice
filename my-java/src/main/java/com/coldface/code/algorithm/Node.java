629127ec59e053d4c548fb70b0198008352a04bf
关心 <jack.zhao829@gmail.com>
2016-01-05 21:09:04 +0800

    add java code

---
+++ my-java/src/main/java/com/coldface/code/algorithm/Node.java
@@ -0,0 +1,16 @@
+package com.coldface.code.algorithm; 
+/** 
+ * 类Node.java的实现描述：链表的实现，每个节点Node都有一个值val和指向下个节点的连接next 
+ * @author coldface 2016年1月5日 下午3:47:18 
+ */
+public class Node {
+    int val;
+    Node next;
+    
+    Node(int x){
+        val=x;
+        next=null;
+    }
+
+}
+
